3cf0e3c324dad2c8a17f9a49727013f1f0b1f8ff
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-03-07 18:27:06 -0800

    Update bad.py

--- bad.py
+++ bad.py
@@ -4,37 +4,45 @@ import sys
 import glob
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
-                    filename='amierrorreport.log',
+                    filename='errorami.log',
                     filemode='a')
 bad_files = []
 good_files = []
 all_files = []
 
-logger = open('test.log','w+') 
+for file in bad_files:
+	all_files.remove(file)
+	good_files = all_files
+	
+logger = open('errorami.log','w+') 
 logger.write('=====================Start=====================\n')
 logger.write('List of AMI factory distribution errors\n')
 
-for bad_file in glob.glob("/var/lib/jenkins/workspace/report_generator/*/outputs.log"):
+for bad_file in glob.glob("/var/lib/jenkins/workspace/copy_test/dianabuild/*/output.test"):
 
 
 	log = open(bad_file)
 	for line in log:
-		if "Error:" in line:
+		if "071279067022" in line:
+			full_log = open(bad_file)
+			for log_line in full_log:
+				logger.write(log_line)
+		elif "156661170251" in line:
 			full_log = open(bad_file)
 			for log_line in full_log:
 				logger.write(log_line)
 
+logger.write('=====================End=====================\n')
 logger.write('=====================Start=====================\n')
 logger.write('List of AMI factory successful distribution:\n')
 
-for good_file in good_files:
+for good_file in glob.glob("/var/lib/jenkins/workspace/copy_test/dianabuild/*/output.test"):
+	
 	full_log = open(good_file)
 	for line in full_log:
-        if "003931215966" in line:
-		full_log = open(good_file)
-		for log_line in full_log:
-			logger.write(log_line)
+		if "031931797306" in line:
+			full_log = open(bad_file)
+			for log_line in full_log:
+				logger.write(log_line
 
-
-logger.write('=====================End=====================\n')
 logger.close()
