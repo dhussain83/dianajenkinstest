36587ca32af36395e0d41192fff5ac486064b553
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-03-07 18:21:38 -0800

    uploaded new code in place of old code.

--- bad.py
+++ bad.py
@@ -4,24 +4,37 @@ import sys
 import glob
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
-                    filename='errorami.log',
+                    filename='amierrorreport.log',
                     filemode='a')
-logger = open('errorami.log','w+') 
+bad_files = []
+good_files = []
+all_files = []
+
+logger = open('test.log','w+') 
 logger.write('=====================Start=====================\n')
 logger.write('List of AMI factory distribution errors\n')
 
-for bad_file in glob.glob("/var/lib/jenkins/workspace/copy_test/dianabuild/*/output.test"):
+for bad_file in glob.glob("/var/lib/jenkins/workspace/report_generator/*/outputs.log"):
 
 
 	log = open(bad_file)
 	for line in log:
-		if "071279067022" in line:
-			full_log = open(bad_file)
-			for log_line in full_log:
-				logger.write(log_line)
-		elif "156661170251" in line:
+		if "Error:" in line:
 			full_log = open(bad_file)
 			for log_line in full_log:
 				logger.write(log_line)
 
+logger.write('=====================Start=====================\n')
+logger.write('List of AMI factory successful distribution:\n')
+
+for good_file in good_files:
+	full_log = open(good_file)
+	for line in full_log:
+        if "003931215966" in line:
+		full_log = open(good_file)
+		for log_line in full_log:
+			logger.write(log_line)
+
+
+logger.write('=====================End=====================\n')
 logger.close()
