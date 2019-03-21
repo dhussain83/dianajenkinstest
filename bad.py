febfc43f489efc6a575ea2ad122130edfb6ba66e
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-03-21 13:29:36 -0700

    Update bad.py

--- bad.py
+++ bad.py
@@ -56,8 +56,8 @@ s3 = boto3.client('s3')
 filename = 'errorami.log'
 bucket_name = 'dianahjenkinsaigbucket'
 directory_name = "errorreports"
-path = os.path.join(directory_name)
-s3.upload_file(filename, bucket_name, filename)
+path = os.path.join(directory_name,filename)
+s3.upload_file(filename, bucket_name, path)
 
 
 #s3 = session.client('s3')
