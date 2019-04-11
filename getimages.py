b57adbeb611b9f7407172898c6a9d1ca5dfcab52
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-04-11 12:20:29 -0700

    Add files via upload

---
+++ getimages.py
@@ -0,0 +1,20 @@
+import boto3
+from operator import itemgetter
+
+client = boto3.client('ec2')
+response = client.describe_images(
+    Filters=[
+        {
+            'Name': 'description',
+            'Values': [
+                'Amazon Linux AMI*',
+            ]
+        },
+    ],
+    Owners=[
+        'amazon'
+    ]
+)
+# Sort on Creation date Desc
+image_details = sorted(response['Images'],key=itemgetter('CreationDate'),reverse=True)
+ami_id = image_details[0]['ImageId']
