6241ab795556000be45c49a0310d9614712fd169
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-04-11 07:48:01 -0700

    Update entitlements.py

--- entitlements.py
+++ entitlements.py
@@ -1,5 +1,17 @@
+import json
 import boto3
 
-client = session.client('marketplace-entitlement', verify=False)
-entitlements = client.get_entitlements(ProductCode='63sbazrz7hre7ensv0gmssh98')
-pp.pprint(entitlements)
+EC2 = boto3.client('ec2')
+response = EC2.describe_images(
+    Owners=['708054772159'], # CentOS
+    Filters=[
+      {'Name': 'name', 'Values': ['CentOS Linux 7 x86_64 HVM EBS *']},
+      {'Name': 'architecture', 'Values': ['x86_64']},
+      {'Name': 'root-device-type', 'Values': ['ebs']},
+    ],
+)
+
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print amis[0]['ImageId']
