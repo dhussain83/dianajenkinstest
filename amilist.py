e4b3cbf955778cbfd2f1078f2f0c0724948ad6ba
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-07 06:54:15 -0700

    Create amilist.py

---
+++ amilist.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+
+EC2 = boto3.client('ec2')
+response = EC2.describe_images(
+    Owners=['309956199498'], # RHEL6.8
+    Filters=[
+      {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA*Access*']},
+)
+
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print amis[0]['ImageId']
