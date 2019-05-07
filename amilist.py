74b83c87a5480f937df65d194919d04fa7a3ba79
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-07 07:18:57 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -1,8 +1,8 @@
 import json
 import boto3
 
-EC2 = boto3.client('ec2')
-response = EC2.describe_images(
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images
     Owners=['309956199498'], # RHEL6.8
     Filters=[
       {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA*Access*']},
