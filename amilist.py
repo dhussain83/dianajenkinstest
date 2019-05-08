b445862eb663c02ec2d675d1efa7e1bf94768153
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-08 07:10:39 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -25,17 +25,6 @@ amis = sorted(response['Images'],
               reverse=True)
 print(amis[0]['ImageId'])
 
-EC2 = boto3.client('ec2', region_name='us-east-1')
-response = EC2.describe_images(
-Owners=['309956199498'], # RHEL6.8
-Filters=[
-  {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA-20*Access*']},
-  ],
-)
-amis = sorted(response['Images'],
-              key=lambda x: x['CreationDate'],
-              reverse=True)
-print(amis[0]['ImageId'])
 
 EC2 = boto3.client('ec2', region_name='us-east-1')
 response = EC2.describe_images(
@@ -49,17 +38,6 @@ amis = sorted(response['Images'],
               reverse=True)
 print(amis[0]['ImageId'])
 
-EC2 = boto3.client('ec2', region_name='us-east-1')
-response = EC2.describe_images(
-Owners=['309956199498'], # RHEL6.8
-Filters=[
-  {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA-20*Access*']},
-  ],
-)
-amis = sorted(response['Images'],
-              key=lambda x: x['CreationDate'],
-              reverse=True)
-print(amis[0]['ImageId'])
 
 EC2 = boto3.client('ec2', region_name='us-east-1')
 response = EC2.describe_images(
