6e7de40b9c3cfe67ae25dab72f9c3d7cd102dc93
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-08 07:09:03 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -24,3 +24,75 @@ amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
 print(amis[0]['ImageId'])
+
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images(
+Owners=['309956199498'], # RHEL6.8
+Filters=[
+  {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA-20*Access*']},
+  ],
+)
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print(amis[0]['ImageId'])
+
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images(
+Owners=['309956199498'], # RHEL6.10
+Filters=[
+  {'Name': 'name', 'Values': ['RHEL-6.10*HVM_GA-20*Access*']},
+  ],
+)
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print(amis[0]['ImageId'])
+
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images(
+Owners=['309956199498'], # RHEL6.8
+Filters=[
+  {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA-20*Access*']},
+  ],
+)
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print(amis[0]['ImageId'])
+
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images(
+Owners=['309956199498'], # RHEL7.4
+Filters=[
+  {'Name': 'name', 'Values': ['RHEL-7.4*HVM_GA-20*Access*']},
+  ],
+)
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print(amis[0]['ImageId'])
+
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images(
+Owners=['309956199498'], # RHEL7.5
+Filters=[
+  {'Name': 'name', 'Values': ['RHEL-7.5_HVM_GA-20*Access*']},
+  ],
+)
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print(amis[0]['ImageId'])
+
+EC2 = boto3.client('ec2', region_name='us-east-1')
+response = EC2.describe_images(
+Owners=['309956199498'], # RHEL7.6
+Filters=[
+  {'Name': 'name', 'Values': ['RHEL-7.6*HVM_GA-20*Access*']},
+  ],
+)
+amis = sorted(response['Images'],
+              key=lambda x: x['CreationDate'],
+              reverse=True)
+print(amis[0]['ImageId'])
