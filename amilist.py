6351b5df03927b58613293d03bfc225a451f85ea
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-08 07:00:37 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -5,7 +5,7 @@ EC2 = boto3.client('ec2', region_name='us-east-1')
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL6.8
 Filters=[
-  {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA*Access*']},
+  {'Name': 'name', 'Values': ['RHEL-6.8_HVM_GA-20*Access*']},
   ],
 )
 amis = sorted(response['Images'],
@@ -17,7 +17,7 @@ EC2 = boto3.client('ec2', region_name='us-east-1')
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL6.9
 Filters=[
-  {'Name': 'name', 'Values': ['RHEL-6.9*HVM_GA*Access*']},
+  {'Name': 'name', 'Values': ['RHEL-6.9*HVM_GA-20*Access*']},
   ],
 )
 amis = sorted(response['Images'],
