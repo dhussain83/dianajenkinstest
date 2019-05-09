e61c5001820afb475f5726504d46ce000a4b1486
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-08 22:50:23 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -15,7 +15,8 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-logger.write(amis[0]['ImageId'])("\n")
+logger.write(amis[0]['ImageId'])
+logger.write("\n")
 
 
 EC2 = boto3.client('ec2', region_name='us-east-1')
