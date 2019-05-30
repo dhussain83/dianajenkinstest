42d72a84e72aad31728c0e26f8fe9795414cb9fa
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-30 10:12:00 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -139,18 +139,18 @@ for ami in amis:
     original_item = table.get_item(
 	Item={
 	   'AMI': ami['Name']
-		}
+	}
+    )
+    if original_item.LatestID == ami['ImageId']:
+	print("no need to update")
+    else:
+	table.put_item(
+	    Item={
+		'AMI': ami['Name'],
+		'LatestID': ami['ImageId']
+	    }
 	)
-	if original_item.LatestID == ami['ImageId']:
-		print("no need to update")
-	else:
-		table.put_item(
-			Item={
-				'AMI': ami['Name'],
-				'LatestID': ami['ImageId']
-			}
-		)
-		ami['Name'].updated = True
+	ami['Name'].updated = True
 #ile1=open("testami.log","r")
 #file2=open("previousamilist/default/testami.log","r")
 #for line1 in file1:
