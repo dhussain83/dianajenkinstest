23218390660078aa753a7f5fff2773ebe85b4edf
Diana Hussain <33631993+dhussain83@users.noreply.github.com>
2019-05-30 07:18:19 -0700

    Update amilist.py

--- amilist.py
+++ amilist.py
@@ -4,9 +4,56 @@ import sys
 import logging
 import difflib
 
+#creating dynamodb table, if not already create
 
-logger = open('testami.log','w+')
+dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
+table_name = 'Latestamis'
+existing_tables = dynamodb_client.list_tables()['TableNames']
 
+if table_name not in existing_tables:
+    response = dynamodb_client.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'AMI',
+                'AttributeType': 'S',
+            },
+            {
+                'AttributeName': 'LatestID',
+                'AttributeType': 'S',
+            },
+        ],
+        KeySchema=[
+            {
+                'AttributeName': 'AMI',
+                'KeyType': 'HASH',
+            },
+            {
+                'AttributeName': 'LatestID',
+                'KeyType': 'RANGE',
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5,
+        },
+        TableName=table_name,
+    )
+
+table = dynamodb.Table('Latestamis')
+
+table = dynamodb.Table('Latestamis')
+
+table.put_item(
+   Item={
+        'username': 'ruanb',
+        'first_name': ImageId'',
+    }
+)
+
+
+#logger = open('testami.log','w+')
+
+#checking RHEL 6.8 ami id
 EC2 = boto3.client('ec2', region_name='us-east-1')
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL6.8
@@ -17,11 +64,11 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-logger.write(amis[0]['ImageId'])
-logger.write("\n")
+#logger.write(amis[0]['ImageId'])
+#logger.write("\n")
 
 
-
+#checking RHEL 6.9 ami id
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL6.9
 Filters=[
@@ -31,12 +78,12 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-#print(amis[0]['ImageId'])
-logger.write(amis[0]['ImageId'])
-logger.write("\n")
+print(amis[0]['ImageId'])
+#logger.write(amis[0]['ImageId'])
+#logger.write("\n")
 
 
-
+#checking RHEL 6.10 ami id
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL6.10
 Filters=[
@@ -46,12 +93,12 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-#print(amis[0]['ImageId'])
-logger.write(amis[0]['ImageId'])
-logger.write("\n")
+print(amis[0]['ImageId'])
+#logger.write(amis[0]['ImageId'])
+#logger.write("\n")
 
 
-
+#checking RHEL 7.4 ami id
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL7.4
 Filters=[
@@ -61,11 +108,11 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-#print(amis[0]['ImageId'])
-logger.write(amis[0]['ImageId'])
-logger.write("\n")
+print(amis[0]['ImageId'])
+#logger.write(amis[0]['ImageId'])
+#logger.write("\n")
 
-
+#checking RHEL 7.5 ami id
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL7.5
 Filters=[
@@ -75,11 +122,11 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-#print(amis[0]['ImageId'])
-logger.write(amis[0]['ImageId'])
-logger.write("\n")
+print(amis[0]['ImageId'])
+#logger.write(amis[0]['ImageId'])
+#logger.write("\n")
 
-
+#checking RHEL 7.6 ami id
 response = EC2.describe_images(
 Owners=['309956199498'], # RHEL7.6
 Filters=[
@@ -89,20 +136,20 @@ Filters=[
 amis = sorted(response['Images'],
               key=lambda x: x['CreationDate'],
               reverse=True)
-#print(amis[0]['ImageId'])
-logger.write(amis[0]['ImageId'])
-logger.close()
+print(amis[0]['ImageId'])
+#logger.write(amis[0]['ImageId'])
+#logger.close()
 
 
-file1=open("testami.log","r")
-file2=open("previousamilist/default/testami.log","r")
-for line1 in file1:
-        for line2 in file2:
-                if line1==line2:
-                        print("No update found for this ami id\n")
-                else:
-                        #send_SNS notification
-                        print("Updated ami id found. Sending sns notification...")
-                break
-file1.close()
-file2.close()
+#ile1=open("testami.log","r")
+#file2=open("previousamilist/default/testami.log","r")
+#for line1 in file1:
+#        for line2 in file2:
+#                if line1==line2:
+#                        print("No update found for this ami id\n")
+#                else:
+#                        #send_SNS notification
+#                        print("Updated ami id found. Sending sns notification...")
+#                break
+#file1.close()
+#file2.close()
